9ace28909a130a06d9b7db7de1c6dbd3fc4aa236
DaveM7788 <david.mcdermott98@gmail.com>
2025-11-02 10:27:24 -0600

    Finished dice fox game
    course completed

--- Scenes/Game/game.gd
+++ Scenes/Game/game.gd
@@ -2,6 +2,18 @@ extends Node2D
 
 const DICE = preload("res://Scenes/Dice/dice.tscn")
 const MARGIN: float = 80.0
+const STOPPABLE: String = "stoppable"
+const GAME_OVER = preload("uid://svpflsu5jowm")
+
+var _points: int = 0
+
+@onready var spawn_timer: Timer = $SpawnTimer
+@onready var score_label: Label = $ScoreLabel
+@onready var music: AudioStreamPlayer2D = $Music
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("restart"):
+		get_tree().reload_current_scene()
 
 func _ready() -> void:
 	spawn_dice()
@@ -17,6 +29,23 @@ func spawn_dice() -> void:
 
 func _on_dice_game_over() -> void:
 	print("Signal for game is over found")
+	pause_all_dice()
+	music.stop()
+	music.stream = GAME_OVER
+	music.play()
+
+func pause_all_dice() -> void:
+	spawn_timer.stop()
+	var stop: Array[Node] = get_tree().get_nodes_in_group(STOPPABLE)
+	for item in stop:
+		item.set_physics_process(false)
+	
 
 func _on_spawn_timer_timeout() -> void:
 	spawn_dice()
+
+
+func _on_fox_point_scored() -> void:
+	print("fox has eaten dice")
+	_points += 1
+	score_label.text = "Score: %04d" % _points
